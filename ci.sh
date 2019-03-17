61a9574323c783769481706b9e310169308d36d5
Alexander Konovalov <alexk@mcs.st-andrews.ac.uk>
2019-03-17 23:09:26 +0000

    Backport updates from the test in the master branch
    - Run package tests in three configurations instead of one
    - Check whether the package is loadable first

--- ci.sh
+++ ci.sh
@@ -59,8 +59,62 @@ ln -s $PWD gaproot/pkg/
 # start GAP with custom GAP root, to ensure correct package version is loaded
 GAP="/home/gap/inst/${GAPDIRNAME}/bin/gap.sh -l $PWD/gaproot; --quitonbreak -q"
 
-# Run package test
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TESTING WHETHER THE PACKAGE IS LOADABLE"
+echo "#"
+echo "######################################################################"
+echo ""
 $GAP <<GAPInput
+Read("/home/gap/travis/ci.g");
+SetInfoLevel(InfoPackageLoading,4);
+if LoadPackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
+    Print("PACKAGE IS NOT LOADABLE - TEST TERMINATED\n");
+    FORCE_QUIT_GAP(1);
+fi;
+QUIT_GAP(0);
+GAPInput
+
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TEST WITH DEFAULT PACKAGES, LOADED AT GAP STARTUP"
+echo "#"
+echo "######################################################################"
+echo ""
+$GAP <<GAPInput
+Read("/home/gap/travis/ci.g");
+if TestOnePackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
+    FORCE_QUIT_GAP(1);
+fi;
+QUIT_GAP(0);
+GAPInput
+
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TEST WITHOUT PACKAGES, EXCEPT REQUIRED BY GAP (using -A option)"
+echo "#"
+echo "######################################################################"
+echo ""
+$GAP -A <<GAPInput
+Read("/home/gap/travis/ci.g");
+if TestOnePackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
+    FORCE_QUIT_GAP(1);
+fi;
+QUIT_GAP(0);
+GAPInput
+
+echo ""
+echo "######################################################################"
+echo "#"
+echo "# TEST WITH ALL PACKAGES LOADED (using LoadAllPackages() command)"
+echo "#"
+echo "######################################################################"
+echo ""
+$GAP <<GAPInput
+LoadAllPackages();
 Read("/home/gap/travis/ci.g");
 if TestOnePackage(LowercaseString(GetNameFromPackageInfo("PackageInfo.g"))) <> true then
     FORCE_QUIT_GAP(1);
